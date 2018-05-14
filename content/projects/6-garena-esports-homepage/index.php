cff3bbcaafbf301cf4200c6033ecba1b759035d3
Jaia Corral <jaiacorral@gmail.com>
2018-05-14 20:48:47 +0800

    test

---
+++ content/projects/6-garena-esports-homepage/index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Esports Homepage</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid"  style="margin-right: 23%; margin-left: 23%;">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">PLACEHOLDER</a>
+            </div>
+
+
+
+
+
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            
+                <ul class="nav navbar-nav navbar-right">
+
+
+                <div class="footer-social-icons">
+    <ul class="social-icons">
+        <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
+    </ul>
+
+
+                    </div>
+                    <li>
+                        <a class="page-scroll" href="#page-top">Home</a>
+                    </li>
+                    <li>
+                        <a href="calendar">Calendar</a>
+                    </li>
+                    <li>
+                        <a href="livestream.php">Livestream</a>
+                    </li>
+                </ul>
+            </div>
+
+
+
+
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <br>
+                <h1 id="homeHeading">Lorem Ipsum<br>Dolor Sit Amet</h1>
+                <p>Vestibulum Id tortor euismod est volutpat interdum at id</p>
+               
+            </div>
+       </div>
+
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <section class="no-padding">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/photogrid/fullsize/1.jpg" class="article-box" style="height: 293.4px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/photogrid/fullsize/2.jpg" class="article-box" style="height: 293.45px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <a href="img/photogrid/fullsize/3.jpg" class="article-box" style="height: 146.42px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <a href="img/photogrid/fullsize/4.jpg" class="article-box" style="height: 146.42px;">                        
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/photogrid/fullsize/5.jpg" class="article-box" style="height: 161.33px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/photogrid/fullsize/6.jpg" class="article-box" style="height: 161.33px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/photogrid/fullsize/7.jpg" class="article-box" style="height: 161.33px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/photogrid/fullsize/8.jpg" class="article-box" style="height: 161.33px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-9 col-sm-6">
+                    <a href="img/photogrid/fullsize/9.jpg" class="article-box" style="height: 293.45px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/photogrid/fullsize/10.jpg" class="article-box" style="height: 293.45px;">
+                        <div class="article-box-caption">
+                            <div class="article-box-caption-content">
+                                <div class="article-category text-faded">
+                                    Category / Title
+                                </div>
+                                <div class="article-name">
+                                    Description
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+                    <a href="#" class="page-scroll btn btn-default btn-xl sr-button">Lorem Ipsum &nbsp &nbsp &nbsp &nbsp &nbsp></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<section id="livestream">
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2><a class="a2" href="livestream">Watch Livestream Now! ></a></h2>
+            </div>
+        </div>
+    </aside>
+</section>
+
+    <section>
+        <div class="container">
+            <div id="footer">
+                <div id="inner">
+                <div class="child">
+                    <h2 class="section-heading">WHAT'S UP?</h2>
+                    <hr class="primary">
+                    <a href="">Lorem Ipsum</a>
+                    <p>Ready to start your next article with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="child">
+                    <h2 class="section-heading">LAST EVENT</h2>
+                    <hr class="primary">
+
+                    <p><img src="img/imgplaceholder.png" align="left" width="60" height="60"> 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p><img src="img/imgplaceholder.png" align="left" width="60" height="60"> 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="child">
+                    <h2 class="section-heading">CONTACTS</h2>
+                    <hr class="primary">
+                    <p>Unit 1201-1202, 12th Floor, Fort Legend Towers, 3rd Avenue, Corner 31st Street, Taguig, 1631 Metro Manila</p>
+                    <div class="footer-social-icons">
+    <ul class="social-icons">
+        <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
+    </ul>
+
+
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="">
+    </section>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
+
---
+++ content/projects/6-garena-esports-homepage/livestream.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Esports Livestream</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstraplivestream.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creativelivestream.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid"  style="margin-right: 23%; margin-left: 23%;">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">PLACEHOLDER</a>
+            </div>
+
+
+
+
+
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            
+                <ul class="nav navbar-nav navbar-right">
+
+
+                <div class="footer-social-icons">
+    <ul class="social-icons">
+        <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
+    </ul>
+
+
+                    </div>
+                    <li>
+                        <a class="page-scroll" href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="calendar">Calendar</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#page-top">Livestream</a>
+                    </li>
+                </ul>
+            </div>
+
+
+
+
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+          
+       
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <section class="no-padding">
+        <div class="container-fluid">
+            <div class="row no-gutter">
+
+                <div class="col-lg-12 col-sm-12 text-center">
+                    <img src="img/livestream/Sample-livestream-vid.jpg" class="fullwidth">
+                </div>
+
+                <div class="col-lg-8 col-sm-12 text-center" style="padding-bottom: 20px;">
+                    <a href="#" class="page-scroll btn btn-default btn-xl sr-button">Lorem Ipsum &nbsp &nbsp &nbsp &nbsp &nbsp></a>
+
+                    &nbsp&nbsp&nbsp&nbsp
+
+                    <a href="#" class="page-scroll btn btn-default btn-xl sr-button">Lorem Ipsum &nbsp &nbsp &nbsp &nbsp &nbsp></a>
+                </div>
+
+                <div class="col-lg-6 col-sm-12 text-center" style="padding-bottom: -10px;">
+                    <h2 class="text-left">Featured Streams</h2>
+                    <img src="img/livestream/stream2.jpg" class="fullwidth">
+                </div>
+
+                <div class="col-lg-6 col-sm-12 text-center" style="padding-bottom: -10px;">
+                    <h2 class="text-left">Featured Streams</h2>
+                    <img src="img/livestream/stream1.jpg" class="fullwidth">
+                </div>
+
+
+
+            </div>
+        </div>
+    </section>
+
+    </div>
+
+    </div> </div> </section> </div> </header>
+    
+<section>
+    <div class="space">
+        
+    </div>
+
+</section>
+
+
+    <section>
+        <div class="container">
+            <div id="footer">
+                <div id="inner">
+                <div class="child">
+                    <h2 class="section-heading">WHAT'S UP?</h2>
+                    <hr class="primary">
+                    <a href="">Lorem Ipsum</a>
+                    <p>Ready to start your next article with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="child">
+                    <h2 class="section-heading">LAST EVENT</h2>
+                    <hr class="primary">
+
+                    <p><img src="img/imgplaceholder.png" align="left" width="60" height="60"> 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p><img src="img/imgplaceholder.png" align="left" width="60" height="60"> 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="child">
+                    <h2 class="section-heading">CONTACTS</h2>
+                    <hr class="primary">
+                    <p>Unit 1201-1202, 12th Floor, Fort Legend Towers, 3rd Avenue, Corner 31st Street, Taguig, 1631 Metro Manila</p>
+                    <div class="footer-social-icons">
+    <ul class="social-icons">
+        <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+        <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
+    </ul>
+
+
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="">
+    </section>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
